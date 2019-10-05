554ef5d3690204df0f983d11beb14b3959cd29c0
Yauheni <Sharikmetal@gmail.com>
2019-10-05 15:12:34 +0300

    Add new method

--- game-test/src/test/java/mytestapp/AppTest.java
+++ game-test/src/test/java/mytestapp/AppTest.java
@@ -5,25 +5,21 @@ import static com.codeborne.selenide.Selenide.*;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
     @Test
     public void shouldUserLogInTheGame() {
+       String user1 = "Test190", user2 = "Test191", user3 = "Test192", password1 = "test190", password2 = "test191", password3 = "test192";
         open("https://ru.kapihospital.com/");
         $(By.id("hotzone_login_head")).click();
-        $(By.name("loginname")).setValue("Test190");
-        $(By.name("password")).setValue("test190");
-        $(By.className("kh_btn")).click();
+        logInUser(user1, password1);
         // should user switch account
-        $(By.id("logout")).click();
-        $(By.name("loginname")).setValue("Test191");
-        $(By.name("password")).setValue("test191");
+        logInUser(user2, password2);
+        logInUser(user3, password3);
+    }
+    public static void logInUser(String login, String password) {
+        $(By.name("loginname")).setValue(login);
+        $(By.name("password")).setValue(password);
         $(By.className("kh_btn")).click();
         $(By.id("logout")).click();
-        $(By.name("loginname")).setValue("Test192");
-        $(By.name("password")).setValue("test192");
-        $(By.className("kh_btn")).click();
     }
 }
